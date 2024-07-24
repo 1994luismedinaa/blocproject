e0baecbbd8f44e8b923f82f928d4718b65358307
Luis Eduardo Medina Huerta <Eduardo_Medina@MacBook-Air-de-Desarrollo-FG.local>
2024-07-24 08:48:12 -0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:blocproject/features/home/ui/home.dart';
+import 'package:flutter/material.dart';
+
+main() {
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
